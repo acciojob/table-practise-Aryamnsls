e9698702ce2496b155e6e75f8dd261168f318ee6
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-03-20 20:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    let tableBody = document.querySelector("#studentTable tbody");
+
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = `
+        <td>Rahul</td>
+        <td>22</td>
+        <td>IT</td>
+    `;
+    tableBody.appendChild(newRow);
+});
